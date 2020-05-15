0e6a9956acf9ec9f3082a323658a6cd86a2352c9
nitinbuzz <tamtech.nitin@pearlcash.com>
2020-05-16 03:57:49 +0530

    init

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: new Date().getTime()};
+  }
+
+  componentDidMount() {
+    fetch("/api/users")
+    .then(response => response.json())
+    .then(data => this.setState({ data }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.data}</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
